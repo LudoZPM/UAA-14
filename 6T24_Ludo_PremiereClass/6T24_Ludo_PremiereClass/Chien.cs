104f0bb2bded155965da9590eb0c9c6243ecc5de
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-09-10 11:25:32 +0200

    POO Exercice 1

---
+++ 6T24_Ludo_PremiereClass/6T24_Ludo_PremiereClass/Chien.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6T24_Ludo_PremiereClass
+{
+    internal class Chien
+    {
+        private string _race;
+        private string _nom;
+        private bool _puce;
+        private double _taille;
+        private double _poids;
+        private string _etat;
+        private int _age;
+
+        public Chien(string race, string nom, double taille, double poids, string etat, int age)
+        {
+            _race = race;
+            _nom = nom;
+            _puce = true;
+            _taille = taille;
+            _poids = poids;
+            _etat = etat;
+            _age = age;
+        }
+        public string Manger()
+        {
+            return "Miam-trop bon";
+        }
+        public string InfosChien()
+        {
+            return "Le chien " + _nom + " ,est de race " + _race + " ,Il a une taille de " + _taille + " cm, il a un poids de " + _poids + " kg, il est :" + _etat + " et ,il a " + _age + " ans";
+        }
+        public string NamesChien()
+        {
+            return _nom;
+        }
+        public string raceChien()
+        {
+            return _race;
+        }
+        public double tailleChien()
+        {
+            return _taille;
+        }
+        public double poidsChien()
+        {
+            return _poids;
+        }
+        public string etatChien()
+        {
+            return _etat;
+        }
+        public int ageChien()
+        {
+            return _age;
+        }
+        
+
+    }
+}
---
+++ 6T24_Ludo_PremiereClass/6T24_Ludo_PremiereClass/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _6T24_Ludo_PremiereClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Chien monPremierChien = new Chien("Bichon", "Flocon", 30 , 6 , "mignon" , 5) ;
+            Console.WriteLine(monPremierChien.InfosChien()) ;
+            Chien[] chiens = new Chien[10];
+            Console.WriteLine(monPremierChien.NamesChien() + monPremierChien.raceChien());
+
+            for (int i = 0; i < chiens.Length; i++)
+            {
+                chiens[i] = new Chien ("race", "nom", i,i, "etat", i) ;
+                Console.WriteLine(chiens[i].NamesChien() + " " + chiens[i].raceChien() + " " + chiens[i].tailleChien() + " " + chiens[i].poidsChien() + " " + chiens[i].etatChien() + " " + chiens[i].ageChien());
+
+            }
+
+            
+
+
+        }
+    }
+}
