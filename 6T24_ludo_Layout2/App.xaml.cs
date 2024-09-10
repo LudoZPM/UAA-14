0afa03b16c23f808e5b2b0b2be98a53585f0153a
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-09-10 17:57:01 +0200

    Layout2 exercice

---
+++ 6T24_ludo_Layout2/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace _6T24_ludo_Layout2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
