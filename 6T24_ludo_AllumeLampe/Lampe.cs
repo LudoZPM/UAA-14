c07a43f8b90ac1bd34c8a43cb7e284a9ac532f86
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-09-16 19:26:13 +0200

    POO lampe exercice

---
+++ 6T24_ludo_AllumeLampe/Lampe.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6T24_ludo_AllumLampe
+{
+    internal class Interrupteur
+    {
+        private string _couleur;
+        private bool _etat;
+        private string _type;
+
+        public Interrupteur(string couleur, string etat, string type)
+        {
+            _couleur = couleur;
+            _etat = true;
+            _type = type;
+        }
+        public string CouleurInterrupteur()
+        {
+            return _couleur;
+        }
+        public bool EtatInterrupteur()
+        {
+            return _etat;
+        }
+        public string TypeInterrupteur()
+        {
+            return _type;
+        }
+        public string InfosInterrupteur()
+        {
+            return " l'interrupteur est de couleur " + _couleur + " , il est de  type" + _type + "." ;
+        }
+    }
+    internal class Lampe
+    {
+        private string _couleur;
+        private string _typeAmpoule;
+        private double _puissance;
+
+        public Lampe(string couleur, string typeAmpoule, double puissance)
+        {
+            _couleur = couleur;
+            _typeAmpoule = typeAmpoule;
+            _puissance = puissance;
+        }
+        public string CouleurLampe()
+        {
+            return _couleur;
+        }
+        public string TypeAmpoule() 
+        { 
+            return _typeAmpoule;
+        }
+        public double PuissanceLampe()
+        {
+            return _puissance;
+        }
+        public string InfosLampe()
+        {
+            return "La lampe est de couleur " + _couleur + " elle a une ampoule de type " + _typeAmpoule + " et elle a une puissance de " + _puissance + "W.";
+        }
+    }
+}
---
+++ 6T24_ludo_AllumeLampe/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _6T24_ludo_AllumLampe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("---Lampe et ampoule---");
+
+            //Lampe maLampe = new Lampe("bleu","led", 20);
+            // Console.WriteLine(maLampe.InfosLampe());
+            Lampe[] lampes = new Lampe[5];
+            Interrupteur[] interrupteurs = new Interrupteur[5];
+            //Console.WriteLine(maLampe.CouleurLampe() + maLampe.TypeAmpoule() + maLampe.PuissanceLampe());
+
+            for (int i = 0; i < lampes.Length; i++) 
+            {
+                lampes[i] = new Lampe("couleur", "type", i);
+                Console.WriteLine(lampes[i].CouleurLampe() + " " + lampes[i].TypeAmpoule() + " " + lampes[i].PuissanceLampe());
+            }
+            
+            for (int i = 0; i < interrupteurs.Length; i++) // Correction ici
+            {
+                interrupteurs[i] = new Interrupteur("couleur", "etat", "type");
+                Console.WriteLine(interrupteurs[i].CouleurInterrupteur() + " " + interrupteurs[i].EtatInterrupteur() + " " + interrupteurs[i].TypeInterrupteur());
+            }
+        }
+    }
+
+}
