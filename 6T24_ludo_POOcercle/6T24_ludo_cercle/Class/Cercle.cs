ce3101f79f883f219c2cd2ae7445e21e1175efa1
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-10-01 10:59:10 +0200

    Exercice poo sur les cercle

---
+++ 6T24_ludo_POOcercle/6T24_ludo_cercle/Class/Cercle.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6T24_ludo_cercle.Class
+{
+    internal class Cercle
+    {
+        private double _rayon;
+
+        public Cercle(double rayon)
+        {
+
+            _rayon = rayon;
+        }
+        public double CalculeAir()
+        {
+            double CalculeAir = Math.PI * Math.Sqrt(_rayon);
+            return CalculeAir;
+
+        }
+        public double CalculePerimetre()
+        {
+            double CalculePerimetre = Math.PI * _rayon * 2;
+            return CalculePerimetre;
+        }
+        public string InfoCercle()
+        {
+            return "Le cercle a un rayon " + _rayon + " a un perimetre de " + CalculePerimetre() + " ";
+        }
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+}
---
+++ 6T24_ludo_POOcercle/6T24_ludo_cercle/Program.cs
@@ -0,0 +1,40 @@
+﻿using _6T24_ludo_cercle.Class;
+
+namespace _6T24_ludo_cercle
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double rayon;
+            String reapetProg;
+
+            do
+            {
+
+
+                Console.WriteLine("entrez le rayon");
+                rayon = Double.Parse(Console.ReadLine());
+                Cercle cercle = new Cercle(rayon);
+
+                Console.WriteLine("air : " + cercle.CalculeAir());
+                Console.WriteLine("perimetre : " + cercle.CalculePerimetre());
+
+
+
+                Console.WriteLine("voulez vous reco (espace)");
+                reapetProg = Console.ReadLine();
+            } while (reapetProg == " ");
+
+            
+            
+            //for (uint i = 0; i < cercle.Length; i++)
+            //{
+                //cercle[i] = new Cercle(i, i);
+                //Console.WriteLine(cercle[i].CalculePerimetre();
+
+            //}
+        }
+    }
+}
