413304605595b2484c233826d8baeaca7cba33af
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-10-08 22:41:18 +0200

    exercice poo act2

---
+++ 6T24_ludo_PooAct2/6T24_ludo_cercle/Class/Premier.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6T24_ludo_cercle.Class
+{
+    internal class Premier
+    {
+        private double _reelle;
+        private double _imaginaire;
+
+        public Premier(double reelle, double imaginaire)
+        {
+            _reelle = reelle;
+            _imaginaire = imaginaire;
+            
+        }
+        
+        public string AfficheComplexe()
+        {
+            string afficheComplexe = "(" + _imaginaire + "," + _reelle + " ) " ;
+            return afficheComplexe;
+        }
+        public double CalculeModule()
+        {
+            double CalculeModule = Math.Sqrt(Math.Pow(_reelle, 2)+ Math.Pow(_imaginaire, 2));
+            return CalculeModule;
+        }
+        public void Ajoute(Complex complexe)
+        {
+            
+        }
+
+    }
+    
+
+}
---
+++ 6T24_ludo_PooAct2/6T24_ludo_cercle/Program.cs
@@ -0,0 +1,67 @@
+﻿using _6T24_ludo_cercle.Class;
+
+namespace _6T24_ludo_cercle
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            double rayon;
+            String reapetProg;
+            string nUser;
+
+            do
+            {
+
+                Console.WriteLine("Entrez ce que vous voulez faire (1: cercle, 2: nombre premier, 3: sandwitch)");
+                nUser = Console.ReadLine();
+
+                if (nUser == "1")
+                {
+                    Console.WriteLine("entrez le rayon (cm)");
+                    rayon = Double.Parse(Console.ReadLine());
+                    Cercle cercle = new Cercle(rayon);
+
+                    Console.WriteLine("air : " + cercle.CalculeAir());
+                    Console.WriteLine("perimetre : " + cercle.CalculePerimetre());
+                    Console.WriteLine("--------------------");
+                    Console.WriteLine("Avec un cercle de rayon  de moitié :");
+                    Cercle cercle2 = new Cercle(rayon / 2);
+                    //Console.WriteLine("Le cercle de rayon " + cercle2.rayon() + ", a un périmètre de " + cercle2.CalculePerimetre() + " et une aire de " + cercle2.CalculeAir());
+
+                }
+                if (nUser == "2")
+                {
+                    Console.WriteLine("Entrez la valur du premier nombre réelle de départ");
+                    Console.WriteLine("Entrez la valur du premier nombre imaginaire de départ");
+                    Console.WriteLine("Entrez la valur du second nombre réelle de départ");
+                    Console.WriteLine("Entrez la valur du second nombre imaginaire de départ");
+
+                }
+                if(nUser == "3")
+                {
+
+                }
+
+
+
+                
+
+
+
+                Console.WriteLine("voulez vous reco (espace)");
+                reapetProg = Console.ReadLine();
+            } while (reapetProg == " ");
+
+            
+            
+            //for (uint i = 0; i < cercle.Length; i++)
+            //{
+                //cercle[i] = new Cercle(i, i);
+                //Console.WriteLine(cercle[i].CalculePerimetre();
+
+            //}
+        }
+    }
+}
