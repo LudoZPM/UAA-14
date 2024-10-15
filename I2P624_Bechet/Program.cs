4969a75fabaa5850898c1afdeffe2c24e54f687a
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-10-15 11:34:43 +0200

    interro feu rouge

---
+++ I2P624_Bechet/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace I2P624_Bechet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string reapet;
+            string nUser;
+
+            do
+            {
+                Console.WriteLine("Hello, World!");
+                Console.WriteLine("feux de signalisation");
+                Console.WriteLine("feu clignotant");
+                Console.WriteLine("changement d etat");
+
+                Console.WriteLine("recommecer ? oui");
+                reapet = Console.ReadLine();
+            } while (reapet == "oui");
+
+            
+        }
+    }
+}
---
+++ I2P624_Bechet/feu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I2P624_Bechet
+{
+    internal class feu
+    {
+        int _couleur;
+        string _identifiant;
+        bool _etat;
+
+    }
+     private string Identifiant()
+    {
+
+        return identifiant;
+    }
+    private bool Etat()
+    {
+
+        return etat;
+    }
+    private void Couleur()
+    {
+
+        return couleur;
+    }
+    
+}
