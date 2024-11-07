c6ab19a0ca09a66f67c536ded384c0720f3b3f5e
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-11-07 14:21:49 +0100

    Frame ex 1

---
+++ 6T24_ludo_FrameEx1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection.Metadata;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using _6T24_ludo_FrameEx1.page;
+
+namespace _6T24_ludo_FrameEx1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Page1_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Frame1();
+        }
+        private void Page2_Click(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Frame2();
+        }
+    }
+}
