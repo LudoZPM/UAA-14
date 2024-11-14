14e53a790caea061eee5e90daa4b764286abeafa
Ludo <138796711+LudoZPM@users.noreply.github.com>
2024-11-14 13:36:22 +0100

    controle

---
+++ UAA14_I4_BechetLudo/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UAA14_I4_BechetLudo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void txtNombre1(object sender, RoutedEventArgs e)
+        {
+
+        }
+        public void txtNombre2(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
