ab8cfa7003fe186a5dad02db18f169af822992e0
Bechet Ludo <220680@CCM-Asty-Moulin.local>
2024-12-05 16:12:58 +0100

    bdd

---
+++ 6T24_ludo_BddEx1/MethodeModel1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+using Org.BouncyCastle.Asn1.Tsp;
+using System.Collections;
+using Org.BouncyCastle.Crypto.Digests;
+
+namespace _6T24_ludo_BddEx1
+{
+    internal class MethodeModel1
+    {
+        public void CD()
+        {
+            MySqlConnection conn = null;
+            MySqlDataAdapter adapter = null;
+            
+        }
+        static string  CheminBDD()
+        {
+            return "server=10.10.51.98; database=immo; port=3306; UserId=root; password=root";
+        }
+        public bool ChercheUtilisateurs (out DataSet infos)
+        {
+            bool ok =false;
+            MySqlConnection maConnexion = new MySqlConnection(CheminBDD());
+            string query = "";
+            try
+            {
+                maConnexion.Open();
+
+                query = "SELECT * FROM utilisateurs;";
+
+                MySqlDataAdapter da = new MySqlDataAdapter(query, maConnexion);
+                infos = new DataSet();
+                da.Fill(infos,"infoUtilisateurs");
+                maConnexion.Close();
+
+                if (infos.Tables[0].Rows.Count >= 1)
+                {
+                    ok = true;
+                }
+               
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+
+            return ok;
+
+        }
+        public bool UpdateBiens (int bienId, string info, string nom)
+        {
+            bool ok = false;
+            MySqlConnection maConnexion = new MySqlConnection(CheminBDD());
+            
+            
+
+            try
+            {
+                string query = "UPDATE table SET nomDuChampAMettreAJour=@parametre WHERE champPourCritere =" + bienId + ";";
+                maConnexion.Open();
+                MySqlCommand upDateCommand = new MySqlCommand(query, maConnexion);
+                upDateCommand.Parameters.AddWithValue("@nom", nom);
+
+
+                if (upDateCommand.ExecuteNonQuery() > 0)
+                {
+                    ok = true;
+                }
+                maConnexion.Open ();
+                return ok;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message);
+                throw;
+            }
+
+
+            return ok;
+
+        }
+        public bool DeleteBiens (int bienId)
+        {
+            bool ok = false;
+            MySqlConnection maConnexion = new MySqlConnection(CheminBDD());
+
+            try
+            {
+                string query = "DELETE table SET nomDuChampAMettreAJour=@parametre WHERE champPourCritere =" + bienId + ";";
+                maConnexion.Open();
+                MySqlCommand upDateCommand = new MySqlCommand(query, maConnexion);
+                upDateCommand.Parameters.AddWithValue("@nom", nom);
+
+            }
+            catch
+            {
+
+            }
+
+            return ok;
+        }
+
+    }
+}
---
+++ 6T24_ludo_BddEx1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _6T24_ludo_BddEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodeModel1 model = new MethodeModel1();  
+            Console.WriteLine("exercice bdd");
+
+        }
+    }
+}
